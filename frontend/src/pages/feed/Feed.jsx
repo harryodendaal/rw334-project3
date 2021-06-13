16a795b4e96bf5eee97fe45e91bf8e629d13eb35
Stephanus Rutger Van Huyssteen <23052228@sun.ac.za>
2021-06-13 19:43:18 +0000

    Fix Feed.jsx

--- frontend/src/pages/feed/Feed.jsx
+++ frontend/src/pages/feed/Feed.jsx
@@ -12,7 +12,8 @@ export const Feed = () => {
       <body>
         <div class="split">
           <div class={styled.split}>
-            <div class="make">
+
+            <div class="make"> 
               <div class={styled.make}>
                 <h2 className={styled.h2create}>Create Post</h2>
                 <div>
@@ -22,8 +23,6 @@ export const Feed = () => {
                   {/* <FetchPosts /> */}
                   <div>
                     <ul>
-<<<<<<< HEAD
-=======
                     {data?.map((post) => (
                       <>
                         <li key={post.id}>
@@ -55,65 +54,31 @@ export const Feed = () => {
                     </form>
                     <p>POSTS</p>
                     <ul className={styled.unorderedList}>
->>>>>>> d65814ce5c882e63ac6dda9fde155eff3edbc8bb
                       {data?.map((post) => (
                         <>
-                          <li key={post.id}>
-                            <Link to={`/post/${post.id}`}>{post.title}</Link>
-                          </li>
+                          <div className={styled.singleBox}>
+                            <h3>
+                              Created by:{post.user} From Group: {post.group}
+                            </h3>
+                            <h4>Created: {post.timestamp}</h4>
+                            <li key={post.id} className={styled.listitem}>
+                              <Link to={`/post/${post.id}`}>{post.title}</Link>
+                            </li>
+                          </div>
                           <br></br>
                         </>
                       ))}
                     </ul>
-                  </div>{" "}
+
+                  </div>
                 </div>
-              </div>
             </div>
 
-            <div class="feed">
-              <div class={styled.feed}>
-                <h2 className={styled.h2}>Posts Feed</h2>
-                <div class={styled.p}>
-                  <form>
-                    <select
-                      className={styled.dropdown}
-                      id="country"
-                      name="country"
-                    >
-                      <option value="desTime">Time (descending)</option>
-                      <option value="ascTime">Time (ascending)</option>
-                      <option value="location">Location</option>
-                      <option value="catagory">Catagory</option>
-                      <option value="user">User</option>
-                      <option value="group">Group</option>
-                    </select>
-                    <button className={styled.buttoN}>
-                      <img src={filter} width="10" height="10" />
-                    </button>
-                  </form>
-                  <p>POSTS</p>
-                  <ul className={styled.unorderedList}>
-                    {data?.map((post) => (
-                      <>
-                        <div className={styled.singleBox}>
-                          <h3>
-                            Created by:{post.user} From Group: {post.group}
-                          </h3>
-                          <h4>Created: {post.timestamp}</h4>
-                          <li key={post.id} className={styled.listitem}>
-                            <Link to={`/post/${post.id}`}>{post.title}</Link>
-                          </li>
-                        </div>
-                        <br></br>
-                      </>
-                    ))}
-                  </ul>
-                </div>
-              </div>
-            </div>
           </div>
         </div>
       </body>
+
+
 
       {/* <div className={styled.container}> 
         <ul className={styled.unorderedList}>
@@ -133,6 +98,8 @@ export const Feed = () => {
           ))}
         </ul>
           </div> */}
+
+
     </>
   );
 };
